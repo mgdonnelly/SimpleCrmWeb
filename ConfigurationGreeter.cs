32451da585f07317b365dd898ca907c1032196f3
mgdonnelly <mgdonnelly@gmail.com>
2020-05-18 10:15:57 -0500

    Commit

---
+++ ConfigurationGreeter.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using SimpleCrm.Web;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace SimpleCrm
+{
+    public class ConfigurationGreeter : IGreeter
+    {
+        public IConfiguration Configuration { get; }
+        public ConfigurationGreeter(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public string GetGreeting()
+        {
+            return Configuration["Greeting"];
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace SimpleCrm.Web
@@ -15,19 +16,26 @@ namespace SimpleCrm.Web
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IGreeter, ConfigurationGreeter>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(
+            IApplicationBuilder app,
+            IHostingEnvironment env,
+            IGreeter greeter)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseWelcomePage();
+
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                var message = greeter.GetGreeting();
+                await context.Response.WriteAsync(message);
             });
         }
     }
